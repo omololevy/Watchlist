fdc03a37a98463cba1624bc268f5ef0f74ef5b39
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-28 02:23:58 +0300

    Define the instance of a movie object

---
+++ app/models/movie.py
@@ -0,0 +1,13 @@
+class Movie:
+  '''
+  Movie class to define the Movie Objects
+  '''
+
+  def __init__(self, id, title, overview,poster,vote_average, vote_count):
+    self.id = id
+    self.title =title
+    self.overview = overview
+    self.poster = 'https://image.tmdb.org/t/p/w500/'+poster
+    self.vote_average = vote_average
+    self.vote_count = vote_count
+      
