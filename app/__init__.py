5d5809156154908c4e208f549e3617d8cf2f1ee7
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-27 12:48:35 +0300

    import DevConfig class from .config module and add the method app.config.from_object()

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 #Flask is a class in flask module
 from flask import Flask
+from .config import DevConfig
 
 #Initializes the application
 app=Flask(__name__)
 
+#Setting up configurations
+app.config.from_object(DevConfig)
 from app import views
