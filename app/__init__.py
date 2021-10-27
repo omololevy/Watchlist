c1ae02a6db8eeca999fbb7580b09eb3a71382e4c
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-27 15:24:54 +0300

    Pass instance relative configuration to connect to config module

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 from .config import DevConfig
 
 #Initializes the application
-app=Flask(__name__)
+app=Flask(__name__, instance_relative_config = True)
 
 #Setting up configurations
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
 from app import views
