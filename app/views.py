2e6314dc9b8d488eb0d589d83b8d3ed6807aaeb3
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-28 12:50:33 +0300

    Add the movie method

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 # 
 from flask import render_template
 from app import app
-
+from .request import get_movies
 #render_template is a function
 
 
@@ -14,3 +14,14 @@ def movie(movie_id):
     title = 'Home - Welcome to the best Movie Review Website Online'
     
     return render_template('movie.html', id = movie_id)
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    #Getting popular movies
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
